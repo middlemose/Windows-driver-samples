febcadce5bda65211d543aa68c2779305f5ca8b4
Matt <138825652+middlemose@users.noreply.github.com>
2024-06-03 11:45:41 -0700

    Test all changes 2

--- .github/scripts/Generate_Files.ps1
+++ .github/scripts/Generate_Files.ps1
@@ -10,10 +10,10 @@ $build_number = "$env:VERBOSE_BUILD_NUMBER"
 # generate Directory.Build.props
 @"
 <Project>
-<Import Project="packages\Microsoft.Windows.WDK.x64.$build_number\build\native\Microsoft.Windows.WDK.x64.props" Condition="Exists('packages\Microsoft.Windows.WDK.x64.$build_number\build\native\Microsoft.Windows.WDK.x64.props') and '$(Platform)' == 'x64'"/>
-<Import Project="packages\Microsoft.Windows.WDK.arm64.$build_number\build\native\Microsoft.Windows.WDK.arm64.props" Condition="Exists('packages\Microsoft.Windows.WDK.arm64.$build_number\build\native\Microsoft.Windows.WDK.arm64.props') and '$(Platform)' == 'ARM64'"/>
-<Import Project="packages\Microsoft.Windows.SDK.CPP.x64.$build_number\build\native\Microsoft.Windows.SDK.cpp.x64.props" Condition="Exists('packages\Microsoft.Windows.SDK.CPP.x64.$build_number\build\native\Microsoft.Windows.SDK.cpp.x64.props') and '$(Platform)' == 'x64'"/>
-<Import Project="packages\Microsoft.Windows.SDK.CPP.arm64.$build_number\build\native\Microsoft.Windows.SDK.cpp.arm64.props" Condition="Exists('packages\Microsoft.Windows.SDK.CPP.arm64.$build_number\build\native\Microsoft.Windows.SDK.cpp.arm64.props') and '$(Platform)' == 'ARM64'"/>
+<Import Project="packages\Microsoft.Windows.WDK.x64.$build_number\build\native\Microsoft.Windows.WDK.x64.props" Condition="Exists('packages\Microsoft.Windows.WDK.x64.$build_number\build\native\Microsoft.Windows.WDK.x64.props') and '`$(Platform)' == 'x64'"/>
+<Import Project="packages\Microsoft.Windows.WDK.arm64.$build_number\build\native\Microsoft.Windows.WDK.arm64.props" Condition="Exists('packages\Microsoft.Windows.WDK.arm64.$build_number\build\native\Microsoft.Windows.WDK.arm64.props') and '`$(Platform)' == 'ARM64'"/>
+<Import Project="packages\Microsoft.Windows.SDK.CPP.x64.$build_number\build\native\Microsoft.Windows.SDK.cpp.x64.props" Condition="Exists('packages\Microsoft.Windows.SDK.CPP.x64.$build_number\build\native\Microsoft.Windows.SDK.cpp.x64.props') and '`$(Platform)' == 'x64'"/>
+<Import Project="packages\Microsoft.Windows.SDK.CPP.arm64.$build_number\build\native\Microsoft.Windows.SDK.cpp.arm64.props" Condition="Exists('packages\Microsoft.Windows.SDK.CPP.arm64.$build_number\build\native\Microsoft.Windows.SDK.cpp.arm64.props') and '`$(Platform)' == 'ARM64'"/>
 <Import Project="packages\Microsoft.Windows.SDK.CPP.$build_number\build\native\Microsoft.Windows.SDK.cpp.props" Condition="Exists('packages\Microsoft.Windows.SDK.CPP.$build_number\build\native\Microsoft.Windows.SDK.cpp.props')"/>
 </Project>
 "@ > "$env:GITHUB_WORKSPACE\Directory.Build.props"
